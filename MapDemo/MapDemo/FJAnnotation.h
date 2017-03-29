43e364616a4c8eb0f94a85f524d1357867f48f6c
MrJamesFeng <812463918@qq.co>
2017-03-29 18:04:40 +0800

    null

--- MapDemo/MapDemo/FJAnnotation.h
+++ MapDemo/MapDemo/FJAnnotation.h
@@ -8,6 +8,7 @@
 
 #import <MapKit/MKFoundation.h>
 #import <MapKit/MKAnnotation.h>
+#warning bug 待解决FJAnnotation 0x17024e220> valueForUndefinedKey:]: this class is not key value coding-compliant for the key latitude
 @interface FJAnnotation : NSObject<MKAnnotation>
 
 @property (nonatomic, copy, nullable) NSString *title;
--- MapDemo/MapDemo/ViewController.m
+++ MapDemo/MapDemo/ViewController.m
@@ -11,7 +11,7 @@
 @import CoreLocation;
 @import MapKit;
 
-@interface ViewController ()<CLLocationManagerDelegate,MKMapViewDelegate,MKAnnotation>
+@interface ViewController ()<CLLocationManagerDelegate,MKMapViewDelegate>
 
 @property(nonatomic,strong)CLLocationManager *locationManager;
 
@@ -37,11 +37,90 @@
     [super viewWillAppear:animated];
     
     [self.view addSubview:self.mapView];
-    
+//    [self.mapView removeAnnotations:self.mapView.annotations];
+//    [self.mapView removeOverlays:self.mapView.overlays];
     [self updateMapDisplay];//调整地图当前合适位置
+    
+//    [self geocoderEvent];//反／地理编码
+    
+    [self regionMonitorEvent];//地理围栏
+    
 
 }
-
+-(void)mapPlan:(MKMapItem *)currentMapItem{
+    
+    
+    //目的地
+    CLLocationCoordinate2D destionationCoordinate = CLLocationCoordinate2DMake(22.58240600, 113.95438400);
+    MKPlacemark *placemark = [[MKPlacemark alloc]initWithCoordinate:destionationCoordinate];
+    MKMapItem *destionaMapItem = [[MKMapItem alloc]initWithPlacemark:placemark];
+    destionaMapItem.name = @"西丽";
+    NSDictionary *launchOptions = @{
+    MKLaunchOptionsDirectionsModeKey:MKLaunchOptionsDirectionsModeDriving,
+    MKLaunchOptionsMapTypeKey:[NSNumber numberWithInt:MKMapTypeStandard]
+                                    };
+    NSArray<MKMapItem *>*mapItems = @[destionaMapItem];
+    BOOL success = [MKMapItem openMapsWithItems:mapItems launchOptions:launchOptions];
+    if (success) {
+        NSLog(@"打开成功");
+    }else{
+        NSLog(@"打开失败");
+    }
+    MKDirectionsRequest *directionsRequest = [[MKDirectionsRequest alloc]init];
+    [directionsRequest setDestination:destionaMapItem];
+    [directionsRequest setSource:currentMapItem];
+    
+    MKDirections *directions = [[MKDirections alloc]initWithRequest:directionsRequest];
+    //添加路线
+    [directions calculateDirectionsWithCompletionHandler:^(MKDirectionsResponse * _Nullable response, NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"error description%@",[error description]);
+        }else{
+            MKRoute *firstRoute = response.routes.firstObject;
+            for (MKRouteStep * routeStep in firstRoute.steps) {
+                NSLog(@"%f %@",routeStep.distance,routeStep.instructions);
+            }
+            
+        }
+    }];
+}
+-(void)geocoderEvent{
+    //地理编码
+    CLGeocoder *geocoder = [[CLGeocoder alloc]init];
+    
+     [geocoder geocodeAddressString:@"深圳前海联动云汽车租赁有限公司" completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+     for (CLPlacemark * mark in placemarks) {
+     NSLog(@"%@",mark);
+     }
+     }];
+//    +22.52388300,+113.91114400 深圳前海联动云汽车租赁有限公司
+    
+    
+    //+22.58240600,+113.95438400 丽新花园
+    CLLocation *location = [[CLLocation alloc]initWithLatitude:22.58240600 longitude:113.95438400];
+    [geocoder reverseGeocodeLocation:location completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"error:%@",[error description]);
+        }
+        for (CLPlacemark * mark in placemarks) {
+            NSLog(@"%@",mark);
+        }
+    }];
+}
+-(void)regionMonitorEvent{
+    //监控能力检测
+    if ([CLLocationManager isMonitoringAvailableForClass:[CLRegion class]]) {
+        NSLog(@"Available OK");
+        //监控位置信息
+        NSSet *regions = [self.locationManager monitoredRegions];
+        for (CLRegion *region in regions) {
+            NSLog(@"region=%@",region);
+        }
+        CLRegion *region = [[CLRegion alloc]initCircularRegionWithCenter:CLLocationCoordinate2DMake(22.52388300, 113.91114400) radius:500 identifier:@"LDY"];
+        [self.locationManager startMonitoringForRegion:region];
+        
+    }
+}
 -(void)updateMapDisplay{
     //调整地图显示位置
     CLLocationCoordinate2D maxCoordinate = CLLocationCoordinate2DMake(-90.0, -180.0);
@@ -129,16 +208,28 @@
     //NSLog(@"horizionAccuracy=%f verticalAccuracy=%f timeStamp=%@ speed=%f direction=%f location.coordinate.latitude=%f location.coordinate.longitude=%f",horizionAccuracy,verticalAccuracy,timeStamp,speed,course,location.coordinate.latitude,location.coordinate.longitude);
     
     [self.mapView setRegion:MKCoordinateRegionMake(location.coordinate,self.coordinateSpan) animated:YES];
-
+    /*
+     //bug 待解决
     FJAnnotation *annotation = [[FJAnnotation alloc]init];
     annotation.coordinate = location.coordinate;
+    [self.mapView addAnnotation:annotation];
+     */
     //[annotation setCoordinate:location.coordinate];
     MKPointAnnotation *pointAnnotation = [[MKPointAnnotation alloc]init];
     pointAnnotation.coordinate = location.coordinate;
     pointAnnotation.title = @"xxxx";
     pointAnnotation.subtitle = @"ooooo";
-    [self.mapView addAnnotation:annotation];
-//    [self.mapView addAnnotation:pointAnnotation];
+    [self.mapView addAnnotation:pointAnnotation];
+    
+    //添加遮盖
+    CLLocationDistance radius = 10;
+    MKCircle *circle = [MKCircle circleWithCenterCoordinate:location.coordinate radius:radius];
+    [self.mapView addOverlay:circle];
+    MKPlacemark *currentPlacemark = [[MKPlacemark alloc]initWithCoordinate:location.coordinate];
+    MKMapItem *currentMapItem = [[MKMapItem alloc]initWithPlacemark:currentPlacemark];
+    [self mapPlan:currentMapItem];//要在地图加载成功后调用
+    
+    
 }
 -(void)locationManagerDidPauseLocationUpdates:(CLLocationManager *)manager{
     
@@ -165,16 +256,62 @@
     }
 }
 #pragma MKMapViewDelegate
--(void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control{
+-(void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control{//CalloutAccessory 为UIControl
     
 }
 -(MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation{
+//    MKPinAnnotationView//继承MKAnnotationView
+//    MKAnnotationView//静态图片作为大头针图片
+    MKPinAnnotationView *pinAnnotationView = (MKPinAnnotationView *)[mapView dequeueReusableAnnotationViewWithIdentifier:@"pin"];
+    if (!pinAnnotationView) {
+      pinAnnotationView = [[MKPinAnnotationView alloc]initWithAnnotation:annotation reuseIdentifier:@"pin"];
+        [pinAnnotationView setPinTintColor:[UIColor greenColor]];
+        [pinAnnotationView setCanShowCallout:YES];
+        [pinAnnotationView setDraggable:YES];
+//        pinAnnotationView.centerOffset = CGPointMake(100, 200);
+//        pinAnnotationView.calloutOffset = CGPointMake(0, -200);//calloutOffset偏离大头针的位置
+
+       
+        
+        
+        //accessory left
+        NSString *imagePath = [[NSBundle mainBundle]pathForResource:@"line.png" ofType:nil];
+        UIImageView *leftView = [[UIImageView alloc]initWithImage:[UIImage imageWithContentsOfFile:imagePath]];
+        [pinAnnotationView setLeftCalloutAccessoryView:leftView];
+        //accessory right
+        UIButton *rightBtn = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+        [pinAnnotationView setRightCalloutAccessoryView:rightBtn];
+    }
+    
+    
+    return pinAnnotationView;
+ 
+    
     if (annotation == mapView.userLocation) {
       return nil;
     }
     return nil;
 }
 -(MKOverlayRenderer *)mapView:(MKMapView *)mapView rendererForOverlay:(id<MKOverlay>)overlay{
+    if ([overlay isKindOfClass:[MKCircle class]]) {
+        
+//        MKCircleView *circleView = [[MKCircleView alloc]initWithOverlay:overlay];
+        
+        MKCircleRenderer *circleRenderer = [[MKCircleRenderer alloc]initWithCircle:overlay];
+        circleRenderer.fillColor = [[UIColor redColor] colorWithAlphaComponent:0.2];
+        circleRenderer.strokeColor = [[UIColor redColor] colorWithAlphaComponent:0.7];
+        circleRenderer.lineWidth = 3.0f;
+        return circleRenderer;
+         
+//        circleView.fillColor = [[UIColor redColor] colorWithAlphaComponent:0.2];
+//        circleView.strokeColor = [[UIColor redColor] colorWithAlphaComponent:0.7];
+//        circleView.lineWidth = 3.0f;
+//        return circleView;
+    }
+   
+    return nil;
+}
+-(MKOverlayView *)mapView:(MKMapView *)mapView viewForOverlay:(id<MKOverlay>)overlay{
     return nil;
 }
 -(void)mapView:(MKMapView *)mapView regionWillChangeAnimated:(BOOL)animated{
@@ -188,6 +325,61 @@
     
 }
 -(void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation{
+    
+}
+-(void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view didChangeDragState:(MKAnnotationViewDragState)newState fromOldState:(MKAnnotationViewDragState)oldState{
+    switch (newState) {
+        case MKAnnotationViewDragStateNone:
+            NSLog(@"MKAnnotationViewDragStateNone");
+            break;
+        case MKAnnotationViewDragStateStarting:
+            NSLog(@"MKAnnotationViewDragStateStarting");
+            break;
+        case MKAnnotationViewDragStateDragging:
+            NSLog(@"MKAnnotationViewDragStateDragging");
+            break;
+        case MKAnnotationViewDragStateCanceling:
+            NSLog(@"MKAnnotationViewDragStateCanceling");
+            break;
+        case MKAnnotationViewDragStateEnding:
+            NSLog(@"MKAnnotationViewDragStateEnding");
+//            [self addActivityIndicatorView:view];
+           
+        {
+            UIActivityIndicatorViewStyle activityIndicatorViewStyle = UIActivityIndicatorViewStyleGray;
+            UIActivityIndicatorView *activityIndicatorView = [[UIActivityIndicatorView alloc]initWithActivityIndicatorStyle:activityIndicatorViewStyle];
+            [activityIndicatorView startAnimating];
+            [view setLeftCalloutAccessoryView:activityIndicatorView];
+            
+//            for (MKCircle *circle in [mapView overlays]) {
+//                circle.coordinate = [view.annotation coordinate];
+//               
+//                [self.mapView addOverlay:circle];
+//            }
+            
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                [activityIndicatorView stopAnimating];
+            });
+        }
+          
+            break;
+            
+        default:
+            break;
+    }
+
+    
+}
+
+
+-(void)addActivityIndicatorView:(MKAnnotationView *)annotationView{
+    UIActivityIndicatorViewStyle activityIndicatorViewStyle = UIActivityIndicatorViewStyleGray;
+    UIActivityIndicatorView *activityIndicatorView = [[UIActivityIndicatorView alloc]initWithActivityIndicatorStyle:activityIndicatorViewStyle];
+    [activityIndicatorView startAnimating];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [activityIndicatorView stopAnimating];
+    });
+    [annotationView setLeftCalloutAccessoryView:activityIndicatorView];
     
 }
 #pragma getter
